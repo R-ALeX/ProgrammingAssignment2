697de4acbd1b57d5385d5c187330c6227bae4553
Alexander <sanek.ryabov@mail.ru>
2020-01-30 22:47:05 +0300

    Add comments

--- New.R
+++ New.R
@@ -1,23 +1,23 @@
-makeCacheMatrix <- function(x = matrix()) {
-        m<-NULL
+makeCacheMatrix <- function(x = matrix()) { #Create Function
+        m<-NULL                             #Create empty variable
         set<-function(y){
-              x<<-y
+              x<<-y                         #Functoin get matrix in cache
               m<<-NULL
         }
-        get<-function() x
-        setinverse <- function() invers <<- solve (x)
-        getinverse <- function() invers
-    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+        get<-function() x                   #assignment function from cache
+        setinverse <- function() invers <<- solve (x)       #Create inverse function and assignment
+        getinverse <- function() invers                     #Call inverse function from cache assignment
+    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)       # Name parametrs for calling
 }
 
-cacheSolve <- function(x, ...) {
-        invers <- x$getInverse()
+cacheSolve <- function(x, ...) {            #Create Function
+        invers <- x$getInverse()            #Assignment inverse function
         if (!is.null(invers)) {
-          message("getting cached data")
+          message("getting cached data")    #Check our cache and get the invers matrix
           return(invers)
         }
-        matrx <- x$get()
-        invers <- solve(matrx, ...)
+        matrx <- x$get()                     
+        invers <- solve(matrx, ...)         #The same procedure as under
         x$setInverse(invers)
         invers
 }
